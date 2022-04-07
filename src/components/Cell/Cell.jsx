c3f3f7ccfc6b8ba60bb58131f45159cb731afcbd
MehrabMP <mehrabmpdev@gmail.com>
2022-04-07 22:49:35 +0430

    Update Cell component

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,19 +1,23 @@
 import classNames from 'classnames';
 import styles from './Cell.module.scss';
 
-const Cell = ({ value, status }) => {
+const Cell = ({ value, status, position, isCompleted }) => {
   const classes = classNames({
     [styles.cell]: true,
     [styles.absent]: status === 'absent',
     [styles.present]: status === 'present',
     [styles.correct]: status === 'correct',
     [styles['cell-fill-animation']]: value,
-    [styles['cell-reveal-animation']]: value,
+    [styles['cell-reveal-animation']]: isCompleted,
   });
 
+  const animationDelay = `${position * 0.35}s`;
+
   return (
-    <div className={classes}>
-      <span className={styles.letter}>{value}</span>
+    <div className={classes} style={{ animationDelay }}>
+      <span className={styles.letter} style={{ animationDelay }}>
+        {value}
+      </span>
     </div>
   );
 };
